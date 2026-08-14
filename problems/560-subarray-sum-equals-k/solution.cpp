892ebc274a1cce31960375d9771ee870d089c958
AnujMadhoria <166337358+AnujMadhoria@users.noreply.github.com>
2026-08-15 00:22:33 +0530

    solve: 560. Subarray Sum Equals K

---
+++ problems/560-subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        vector<int> prefixSum(nums.size(), 0);
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        unordered_map<int, int> mp;
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (prefixSum[i] == k) {
+                ans++;
+            }
+            if (mp.find(prefixSum[i] - k) != mp.end()) {
+                ans += mp[prefixSum[i] - k];
+            }
+            if (mp.find(prefixSum[i]) == mp.end()) {
+                mp[prefixSum[i]] = 0;
+            }
+            mp[prefixSum[i]]++;
+        }
+        return ans;
+    }
+};
